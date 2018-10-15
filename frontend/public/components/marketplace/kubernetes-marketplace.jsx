0178306e43bb87eec44c3a82e6dab42a45d87fea
Lance <lgallett@redhat.com>
2018-10-15 14:30:20 -0400

    Updating: adding modal overlay

--- frontend/public/components/marketplace/kubernetes-marketplace.jsx
+++ frontend/public/components/marketplace/kubernetes-marketplace.jsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import * as _ from 'lodash-es';
 import * as PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
+import CatalogTileView from 'patternfly-react-extensions/dist/esm/components/CatalogTileView/CatalogTileView';
+import CatalogTile from 'patternfly-react-extensions/dist/esm/components/CatalogTile/CatalogTile';
 
 import {Firehose, PageHeading, StatusBox} from '../utils';
 import {referenceForModel} from '../../module/k8s';
 import {normalizeIconClass} from '../catalog-item-icon';
 import {PackageManifestModel} from '../../models';
-import CatalogTileView from 'patternfly-react-extensions/dist/esm/components/CatalogTileView/CatalogTileView';
-import CatalogTile from 'patternfly-react-extensions/dist/esm/components/CatalogTile/CatalogTile';
+import {MarketplaceModalOverlay} from './modal-overlay';
 
 const normalizePackageManifests = (packageManifests, kind) => {
   const activePackageManifests = _.filter(packageManifests, packageManifest => {
@@ -20,18 +21,27 @@ const normalizePackageManifests = (packageManifests, kind) => {
     const iconObj = _.get(packageManifest, 'status.channels[0].currentCSVDesc.icon[0]');
     const tileImgUrl = iconObj && `data:${iconObj.mediatype};base64,${iconObj.base64data}`;
     const tileIconClass = tileImgUrl ? null : iconClass;
-    const tileDescription = packageManifest.metadata.description;
-    const tileProvider = _.get(packageManifest, 'metadata.labels.provider');
+    const provider = _.get(packageManifest, 'metadata.labels.provider');
     const tags = packageManifest.metadata.tags;
+    const version = _.get(packageManifest, 'status.channels[0].currentCSVDesc.version');
+    const currentCSVAnnotations = _.get(packageManifest, 'status.channels[0].currentCSVDesc.annotations', {});
+    const { description, certifiedLevel, healthIndex, repository, containerImage, createdAt, support } = currentCSVAnnotations;
     return {
       obj: packageManifest,
       kind,
       tileName,
       tileIconClass,
       tileImgUrl,
-      tileDescription,
-      tileProvider,
+      description,
+      provider,
       tags,
+      version,
+      certifiedLevel,
+      healthIndex,
+      repository,
+      containerImage,
+      createdAt,
+      support,
     };
   });
 };
@@ -49,7 +59,9 @@ const getItems = (props) => {
 class MarketplaceListPage extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      selectedTile: null
+    };
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -60,16 +72,23 @@ class MarketplaceListPage extends React.Component {
     }
   }
 
+  toggleOpen(item) {
+    this.setState(prevState => {
+      const selectedTile = prevState.selectedTile === item ? null : item;
+      return { selectedTile };
+    });
+  }
+
   renderTiles() {
     const {items} = this.state;
 
     return (
       <CatalogTileView.Category totalItems={items.length} viewAll={true}>
         {_.map(items, ((item) => {
-          const {obj, tileName, tileImgUrl, tileIconClass, tileProvider, tileDescription} = item;
+          const {obj, tileName, tileImgUrl, tileIconClass, provider, description} = item;
           const uid = obj.metadata.uid;
           const iconClass = tileIconClass ? `icon ${normalizeIconClass(tileIconClass)}` : null;
-          const vendor = tileProvider ? `Provided by ${tileProvider}` : null;
+          const vendor = provider ? `Provided by ${provider}` : null;
           return <CatalogTile
             id={uid}
             key={uid}
@@ -77,7 +96,8 @@ class MarketplaceListPage extends React.Component {
             iconImg={tileImgUrl}
             iconClass={iconClass}
             vendor={vendor}
-            description={tileDescription}
+            description={description}
+            onClick={() => this.toggleOpen(item)}
           />;
         }))}
       </CatalogTileView.Category>
@@ -86,7 +106,7 @@ class MarketplaceListPage extends React.Component {
 
   render() {
     const {loaded, loadError} = this.props;
-    const {items} = this.state;
+    const {items, selectedTile} = this.state;
     return <StatusBox data={items} loaded={loaded} loadError={loadError} label="Resources">
       <div className="co-catalog-page">
         <div className="co-catalog-page__content">
@@ -94,6 +114,8 @@ class MarketplaceListPage extends React.Component {
           <CatalogTileView>
             {this.renderTiles()}
           </CatalogTileView>
+          {selectedTile &&
+          <MarketplaceModalOverlay item={selectedTile} close={() => this.toggleOpen(null)} openSubscribe={/* TODO */} />}
         </div>
       </div>
     </StatusBox>;
---
+++ frontend/public/components/marketplace/modal-overlay.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'patternfly-react/dist/esm/components/Button/Button';
+import Modal from 'patternfly-react/dist/esm/components/Modal/Modal';
+import CatalogItemHeader from 'patternfly-react-extensions/dist/esm/components/CatalogItemHeader/CatalogItemHeader';
+import PropertiesSidePanel from 'patternfly-react-extensions/dist/esm/components/PropertiesSidePanel/PropertiesSidePanel';
+import PropertyItem from 'patternfly-react-extensions/dist/esm/components/PropertiesSidePanel/PropertyItem';
+
+const MarketplaceModalOverlay = (props) => {
+  const { item, close /* openSubscribe */ } = props;
+  const { tileName, tileImgUrl, provider, description, version, certifiedLevel, healthIndex, repository, containerImage, createdAt, support } = item;
+  const notAvailable = <span className="properties-side-panel-pf-property-label">N/A</span>;
+  return (
+    <React.Fragment>
+      <Modal show={true} className="right-side-modal-pf" bsSize={'lg'}>
+        <Modal.Header>
+          <CatalogItemHeader
+            className="co-marketplace-modal__item-header"
+            iconImg={tileImgUrl}
+            title={tileName}
+            vendor={<span> {provider}</span>}
+          />
+          <Modal.CloseButton onClick={close} />
+        </Modal.Header>
+        <Modal.Body>
+          <div className="co-marketplace-modal__body">
+            <PropertiesSidePanel>
+              <Button bsStyle="primary" className="co-marketplace-modal__subscribe" /* onClick={ openSubscribe }*/ >
+                Subscribe
+              </Button>
+              <PropertyItem label="Operator Version" value={version || notAvailable} />
+              <PropertyItem label="Certified Level" value={certifiedLevel || notAvailable} />
+              <PropertyItem label="Provider" value={provider || notAvailable} />
+              <PropertyItem label="Health Index" value={healthIndex || notAvailable} />
+              <PropertyItem label="Repository" value={repository || notAvailable} />
+              <PropertyItem label="Container Image" value={containerImage || notAvailable} />
+              <PropertyItem label="Created At" value={createdAt || notAvailable} />
+              <PropertyItem label="Support" value={support || notAvailable} />
+            </PropertiesSidePanel>
+            <div className="co-marketplace-modal__item co-marketplace-modal__description">
+              {description}
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </React.Fragment>
+  );
+};
+MarketplaceModalOverlay.propTypes = {
+  size: PropTypes.string
+};
+MarketplaceModalOverlay.defaultProps = {
+  size: 'default'
+};
+export { MarketplaceModalOverlay };
