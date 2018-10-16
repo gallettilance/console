35981c8c63b1c1024c59643e7c4ce0535cce77c3
Jeffrey Phillips <jephilli@redhat.com>
2018-10-16 13:24:11 -0400

    Updates to catalog details modal structure

--- frontend/public/components/marketplace/modal-overlay.jsx
+++ frontend/public/components/marketplace/modal-overlay.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal } from 'patternfly-react';
-import { CatalogItemHeader, PropertiesSidePanel, PropertyItem } from 'patternfly-react-extensions';
+
+import Button from 'patternfly-react/dist/esm/components/Button/Button';
+import Modal from 'patternfly-react/dist/esm/components/Modal/Modal';
+import CatalogItemHeader from 'patternfly-react-extensions/dist/esm/components/CatalogItemHeader/CatalogItemHeader';
+import PropertiesSidePanel from 'patternfly-react-extensions/dist/esm/components/PropertiesSidePanel/PropertiesSidePanel';
+import PropertyItem from 'patternfly-react-extensions/dist/esm/components/PropertiesSidePanel/PropertyItem';
 
 class MarketplaceModalOverlay extends React.Component {
   constructor(props){
@@ -10,37 +14,34 @@ class MarketplaceModalOverlay extends React.Component {
   render() {
     const {item, close /* openSubscribe */} = this.props;
     const {tileName, tileImgUrl, tileProvider, tileDescription, version, certifiedLevel, healthIndex, repository, containerImage, createdAt, support} = item;
+    const notAvailable = <span className="properties-side-panel-pf-property-label">N/A</span>;
     return (
       <React.Fragment>
         <Modal show={true} className="right-side-modal-pf" bsSize={'lg'}>
           <Modal.Header>
-            <Modal.CloseButton onClick={close} />
-          </Modal.Header>
-          <Modal.Body>
             <CatalogItemHeader
-              className="long-description-test"
+              className="co-marketplace-modal--item-header"
               iconImg={tileImgUrl}
               title={tileName}
               vendor={<span> {tileProvider}</span>}
             />
-            <div className="co-marketplace-modal">
-              <div className="co-marketplace-modal--item co-marketplace-modal--properties__border">
-                <Button bsStyle="primary" style={{ width: '100%' }} onClick={/* openSubscribe */}>
+            <Modal.CloseButton onClick={close} />
+          </Modal.Header>
+          <Modal.Body>
+            <div className="co-marketplace-modal--body">
+              <PropertiesSidePanel>
+                <Button bsStyle="primary" className="co-marketplace-modal--subscribe" /* onClick={ openSubscribe }*/ >
                   Subscribe
                 </Button>
-                <br />
-                <br></br>
-                <PropertiesSidePanel>
-                  <PropertyItem key={'ver'} label={'Operator Version'} value={version} />
-                  <PropertyItem key={'ver'} label={'Certified Level'} value={certifiedLevel} />
-                  <PropertyItem key={'pro'} label={'Provider'} value={tileProvider} />
-                  <PropertyItem key={'ver'} label={'Health Index'} value={healthIndex} />
-                  <PropertyItem key={'ver'} label={'Repository'} value={repository} />
-                  <PropertyItem key={'ver'} label={'Container Image'} value={containerImage} />
-                  <PropertyItem key={'ver'} label={'Created At'} value={createdAt} />
-                  <PropertyItem key={'ver'} label={'Support'} value={support} />
-                </PropertiesSidePanel>
-              </div>
+                <PropertyItem label={'Operator Version'} value={version || notAvailable} />
+                <PropertyItem label={'Certified Level'} value={certifiedLevel || notAvailable} />
+                <PropertyItem label={'Provider'} value={tileProvider || notAvailable} />
+                <PropertyItem label={'Health Index'} value={healthIndex || notAvailable} />
+                <PropertyItem label={'Repository'} value={repository || notAvailable} />
+                <PropertyItem label={'Container Image'} value={containerImage || notAvailable} />
+                <PropertyItem label={'Created At'} value={createdAt || notAvailable} />
+                <PropertyItem label={'Support'} value={support || notAvailable} />
+              </PropertiesSidePanel>
               <div className="co-marketplace-modal--item co-marketplace-modal--description">
                 {tileDescription}
               </div>
@@ -48,7 +49,8 @@ class MarketplaceModalOverlay extends React.Component {
           </Modal.Body>
         </Modal>
       </React.Fragment>
-    )};
+    );
+  }
 }
 MarketplaceModalOverlay.propTypes = {
   size: PropTypes.string
